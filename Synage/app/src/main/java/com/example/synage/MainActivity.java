7aebf3e56aadb59b4b80b17018c2c82cc94acfe4
GueTorres <guevara.torres619@outlook.com>
2019-10-19 10:09:09 -0400

    change fab to camera button

--- Synage/app/src/main/java/com/example/synage/MainActivity.java
+++ Synage/app/src/main/java/com/example/synage/MainActivity.java
@@ -22,14 +22,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -56,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void Syn(View view) {
 
-        Intent intent = new Intent();
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
     }
 }
